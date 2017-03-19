0298a054a03d636641c0eb10ee777c9d9a44f34f
Allan Tokuda <praetis@gmail.com>
2017-03-19 13:37:44 -0500

    Fairly smart date finder

--- src/birthday-spec.js
+++ src/birthday-spec.js
@@ -1,15 +1,64 @@
-describe('dateFromString', function() {
+describe('datesFromString', function() {
   describe('mm/dd/yyyy format', function() {
     beforeEach(function() {
       FORMAT = ['mm', 'dd', 'yyyy'];
     });
 
     it('returns no matches when provided an empty string', function() {
-      expect(dateFromString(FORMAT, '')).toEqual({});
+      results = datesFromString(FORMAT, '');
+      expect(results.length).toEqual(0);
     });
 
     it('finds fully typed dates without separators', function() {
-      expect(dateFromString(FORMAT, '08131984')).toEqual({ month: '08', day: '13', year: '1984' });
+      results = datesFromString(FORMAT, '08131984');
+      expect(results.length).toEqual(1);
+      expect(results[0]).toEqual({ month: 8, day: 13, year: 1984 });
+    });
+
+    it('finds dates when missing zeros', function() {
+      results = datesFromString(FORMAT, '871984');
+      expect(results.length).toEqual(1);
+      expect(results[0]).toEqual({ month: 8, day: 7, year: 1984 });
+    });
+
+    it('finds multiple results when ambiguous', function() {
+      results = datesFromString(FORMAT, '1111984');
+      expect(results.length).toEqual(2);
+      expect(results[0]).toEqual({ month: 1, day: 11, year: 1984 });
+      expect(results[1]).toEqual({ month: 11, day: 1, year: 1984 });
+    });
+
+  });
+
+  describe('findNumberInString', function() {
+    beforeEach(function() {
+      /* map() wrapper for testing*/
+      resultValues = function(str, i, digits, min, max) {
+        return findNumberInString(str, i, digits, min, max).map(function(result) { return result.value });
+      }
+    });
+
+    it('finds numbers in a string and the character length occupied by the found numbers', function() {
+      expect(findNumberInString('0123456789', 4)).toEqual([
+        { value: 1, length: 2 },
+        { value: 12, length: 3 },
+        { value: 123, length: 4 }
+      ]);
+    });
+
+    it('filters results based on a max value', function() {
+      expect(resultValues('010000', 2, 1, 31)).toEqual([1]);
+      expect(resultValues('300000', 2, 1, 31)).toEqual([3, 30]);
+      expect(resultValues('310000', 2, 1, 31)).toEqual([3, 31]);
+      expect(resultValues('320000', 2, 1, 31)).toEqual([3]);
+      expect(resultValues('410000', 2, 1, 31)).toEqual([4]);
+
+      expect(resultValues('191700', 4, 1918, 2017)).toEqual([]);
+      expect(resultValues('191800', 4, 1918, 2017)).toEqual([1918]);
+      expect(resultValues('199800', 4, 1918, 2017)).toEqual([1998]);
+      expect(resultValues('201700', 4, 1918, 2017)).toEqual([2017]);
+      expect(resultValues('201800', 4, 1918, 2017)).toEqual([]);
     });
   });
+
 });
--- src/birthday.js
+++ src/birthday.js
@@ -2,35 +2,84 @@ finders = {
   dd: {
     key: 'day',
     find: function(str, i) {
-      return [str.substr(i, 2), i + 2];
+      return findNumberInString(str, 2, 1, 31); // TODO: limit to 28 for February, etc.
     }
   },
 
   mm: {
     key: 'month',
     find: function(str, i) {
-      return [str.substr(i, 2), i + 2];
+      return findNumberInString(str, 2, 1, 12);
     }
   },
 
   yyyy: {
     key: 'year',
     find: function(str, i) {
-      return [str.substr(i, 4), i + 4];
+      return findNumberInString(str, 4, 1918, 2017);
     }
   }
 }
 
-dateFromString = function(format, str) {
-  i = 0;
-  result = {};
-  format.forEach(function(part) {
-    if (i < str.length) {
-      f = finders[part];
-      [value, i] = f.find(str, i);
-      result[f.key] = value;
+
+datesFromString = function(format, str) {
+  if (format.length == 0) return [];
+
+  var dates = [];
+
+  var thisFormat      = format[0];
+  var remainderFormat = format.slice(1);
+  var key = finders[thisFormat].key;
+
+  finders[thisFormat].find(str).forEach(function(partResult) {
+    var remainderString = str.slice(partResult.length);
+
+    datesFromString(remainderFormat, remainderString).forEach(function(date) {
+      date[key] = partResult.value;
+      dates.push(date);
+    });
+
+    if (dates.length == 0) {
+      var date = {}
+      date[key] = partResult.value;
+      dates.push(date);
     }
   });
-  return result;
+
+  var bestDates = [];
+
+  var maxKeys = 0;
+  dates.forEach(function(date) {
+    numKeys = Object.keys(date).length;
+    if (numKeys > maxKeys) {
+      maxKeys = numKeys;
+    }
+  });
+  dates.forEach(function(date) {
+    numKeys = Object.keys(date).length;
+    if (numKeys == maxKeys) {
+      bestDates.push(date);
+    }
+  });
+
+  return bestDates;
 }
+
+
+/* Find all integers that can be extracted start at the beginning of `searchString`, within the given number of digits and min/max range. */
+findNumberInString = function(searchString, numDigits, min = 1, max = Infinity) {
+  results = [];
+  for(length=1; length<=numDigits; length++) {
+    testString = searchString.substr(0, length);
+    num = parseInt(testString, 10);
+    if (num >= min && num <= max) {
+      results.push({
+        value: num,
+        length: length
+      });
+    }
+  }
+  return results;
+}
+
 
